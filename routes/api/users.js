990c27239605dd9e8ce6cb6513f69d4d49d2b644
shrijan <shrijan00003@gmail.com>
2018-07-29 03:17:13 -0400

    JWT IS USED IN USER API

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
 
 
 
@@ -48,10 +50,10 @@ router.post('/register', (req, res) => {
                 });
 
                 //using bcrypt to hash password
-                bcrypt.genSalt(10,(err,salt)=>{
-                    bcrypt.hash(newUser.password, salt, (err,hash)=>{
-                        if(err) throw err;
-                        newUser.password =  hash;
+                bcrypt.genSalt(10, (err, salt) => {
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        if (err) throw err;
+                        newUser.password = hash;
                         newUser.save()
                             .then(user => res.json(user))
                             .catch(err => console.log(err));
@@ -61,5 +63,59 @@ router.post('/register', (req, res) => {
         })
 })
 
+/**
+ * @route GET api/users/login
+ * @description Login user  /Return JWT Token
+ * @access Public
+ */
+
+router.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    //find user by email
+
+    User.findOne({ email })
+        .then(user => {
+            if (!user) {
+                return res.status(404).json({
+                    email: 'User not found'
+                });
+            }
+
+            //check password
+            bcrypt.compare(password, user.password)
+                .then(isMatch => {
+                    if (isMatch) {
+
+                        //user matched
+                        //  res.json({msg: 'Success'})
+
+                        //create payload = payload is the information used for creating hash password so that can be compared later for verifying it 
+
+                        const payload = { id: user.id, name: user.name, avatar: user.avatar }
+
+                        //sign Token
+
+                        jwt.sign(
+                            payload, 
+                            keys.secretOrKey, 
+                            { expiresIn: 3600 }, 
+                            (err, token) => {
+                                res.json({
+                                    success : true,
+                                    token :'Bearer '+ token 
+                                })
+                        });
+
+
+                    } else {
+                        return res.status(400).json({
+                            password: 'Password Incorrect'
+                        })
+                    }
+                })
+        })
+})
 
 module.exports = router;
